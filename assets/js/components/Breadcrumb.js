36446fe6e2d14a3125b3459ce0c1ab0e75c4cfb1
katre <0mica4420@gmail.com>
2018-08-21 13:53:05 +0200

    Indent properly

--- assets/js/components/Breadcrumb.js
+++ assets/js/components/Breadcrumb.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 const FormBreadcrumb = (props) => {
-    return (
-        <div>
-            <Breadcrumb>
-                <BreadcrumbItem active>Commande </BreadcrumbItem>
-                <BreadcrumbItem><a href="#">Tickets </a></BreadcrumbItem>
-                <BreadcrumbItem ><a href="#">Paiement </a></BreadcrumbItem>
-            </Breadcrumb>
-        </div>
-    );
+  return (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbItem active>Commande </BreadcrumbItem>
+        <BreadcrumbItem><a href="#">Tickets </a></BreadcrumbItem>
+        <BreadcrumbItem ><a href="#">Paiement </a></BreadcrumbItem>
+      </Breadcrumb>
+    </div>
+  );
 };
 
 export default FormBreadcrumb;
