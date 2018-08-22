651cc7a4ab9d374bb15276a39d0fe4487de4f5c1
katre <0mica4420@gmail.com>
2018-08-22 19:00:41 +0200

    better breadcrumb

--- assets/js/components/Breadcrumb.js
+++ assets/js/components/Breadcrumb.js
@@ -4,11 +4,22 @@ import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 const FormBreadcrumb = (props) => {
   return (
     <div>
-      <Breadcrumb>
+      {/* <Breadcrumb>
         <BreadcrumbItem active>Commande </BreadcrumbItem>
         <BreadcrumbItem><a href="#">Tickets </a></BreadcrumbItem>
         <BreadcrumbItem ><a href="#">Paiement </a></BreadcrumbItem>
-      </Breadcrumb>
+      </Breadcrumb> */}
+      <ul className="nav nav-pills card-header-pills mx-auto my-5" style={{maxWidth: 500}}>
+        <li className="nav-item">
+          <a className="nav-link active" href="#">Détails de la commande</a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link disabled">Choix des tickets</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#">Paiement</a>
+        </li>
+      </ul>
     </div>
   );
 };
