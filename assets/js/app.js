af170e90df297a304ea9832ddc4fb7297f84813b
katre <0mica4420@gmail.com>
2018-08-27 16:36:17 +0200

    modernize breadcrumb

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import {Container} from 'reactstrap'
 
 import FormContainer from './components/FormContainer'
-import Breadcrumb from './components/Breadcrumb'
 
 import 'react-day-picker/lib/style.css'
 import '../css/app.css'
@@ -13,7 +12,6 @@ class App extends React.Component {
     render(){
         return(
             <Container>
-                <Breadcrumb />
                 <FormContainer />
             </Container>
         )
--- assets/js/components/Breadcrumb.js
+++ assets/js/components/Breadcrumb.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Nav, NavItem, NavLink, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
-const FormBreadcrumb = (props) => {
+const FormBreadcrumb = props => {
   return (
     <div>
-      {/* <Breadcrumb>
-        <BreadcrumbItem active>Commande </BreadcrumbItem>
-        <BreadcrumbItem><a href="#">Tickets </a></BreadcrumbItem>
-        <BreadcrumbItem ><a href="#">Paiement </a></BreadcrumbItem>
-      </Breadcrumb> */}
-      <ul className="nav nav-pills card-header-pills mx-auto my-5" style={{maxWidth: 500}}>
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Détails de la commande</a>
-        </li>
-        <li className="nav-item">
-          <a href="#" className="nav-link disabled">Choix des tickets</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#">Paiement</a>
-        </li>
-      </ul>
+      <Nav pills style={{maxWidth: 465, borderRadius: 6, backgroundColor: 'whitesmoke',}} className=" my-5">
+        <NavItem>
+          <NavLink href="#" onClick={() => props.showForm(1)} active={props.show === 1}> Détails de la commande </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#" onClick={() => props.showForm(2)} active={props.show === 2} disabled={props.show < 2} >   
+            Choix des tickets 
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#" onClick={() => props.showForm(3)} active={props.show === 3} disabled={props.show < 3}>
+            Paiement 
+          </NavLink>
+        </NavItem>
+      </Nav>
     </div>
   );
 };
