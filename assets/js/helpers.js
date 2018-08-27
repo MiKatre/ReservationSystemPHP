cf58a0a2f17dba12b8857f0d66a34a600dafecef
katre <0mica4420@gmail.com>
2018-08-27 16:38:46 +0200

    extend helpers

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -2,11 +2,17 @@ const getAge = (birthDate) => {
 	return (new Date() - birthDate) / 1000 / 3600 / 24 / 365;
 }
 
-export const getPrice = (birthDate) => {
-	const age = getAge(birthDate);
-	const normal = 1600; 	// à partir de 12 ans
-	const children = 800; 	// de 4 à 12 ans
-	const senior = 1200; 	// à partir de 60 ans
+export const getRate = (birthDate) => {
+	const age = getAge(birthDate)
+	const normal = {};		// à partir de 12 ans
+	normal.price = 1600;
+	normal.name = 'Normal';
+	const children = {}; 	// de 4 à 12 ans
+	children.price = 800; 	
+	children.name = 'Enfant';
+	const senior = {}; 		// à partir de 60 ans
+	senior.price = 1200;
+	senior.name = 'Senior';
 
 	return age >= 4 && age <= 12 
 	? children
