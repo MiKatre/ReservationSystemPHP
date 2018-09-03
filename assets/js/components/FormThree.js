2e4912d70533901618608852c88b2c0032863716
katre <0mica4420@gmail.com>
2018-09-03 15:10:20 +0200

    create checkout form

--- assets/js/components/FormThree.js
+++ assets/js/components/FormThree.js
@@ -1,7 +1,141 @@
 import React from 'react'
 
-const FormThree = () => (
-  <p>Form Three</p>
-)
+// import ReactStripeElements from 'react-stripe-elements';
+// import InjectedCheckoutForm from './CheckoutForm/CheckoutForm'
 
-export default FormThree
+// const FormThree = () => (
+//   <Elements>
+//     <InjectedCheckoutForm />
+//   </Elements>
+// )
+
+// export default FormThree
+
+import {
+  CardNumberElement,
+  CardExpiryElement,
+  CardCVCElement,
+  PostalCodeElement,
+  Elements,
+  injectStripe,
+} from 'react-stripe-elements';
+
+const handleBlur = () => {
+  console.log('[blur]');
+};
+const handleChange = (change) => {
+  console.log('[change]', change);
+};
+const handleFocus = () => {
+  console.log('[focus]');
+};
+const handleReady = () => {
+  console.log('[ready]');
+};
+
+const createOptions = (fontSize, padding) => {
+  return {
+    style: {
+      base: {
+        fontSize,
+        color: '#424770',
+        letterSpacing: '0.025em',
+        fontFamily: 'Source Code Pro, monospace',
+        '::placeholder': {
+          color: '#aab7c4',
+        },
+        padding,
+      },
+      invalid: {
+        color: '#9e2146',
+      },
+    },
+  };
+};
+
+class _SplitForm extends React.Component {
+  handleSubmit(ev) {
+    ev.preventDefault();
+    if (this.props.stripe) {
+      this.props.stripe
+        .createToken()
+        .then((payload) => console.log('[token]', payload));
+    } else {
+      console.log("Stripe.js hasn't loaded yet.");
+    }
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="mb-3">
+          <label className="stripe-label">
+            Numéro de carte
+            <CardNumberElement
+              onBlur={handleBlur}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onReady={handleReady}
+              {...createOptions(this.props.fontSize)}
+            />
+          </label>
+        </div>
+        <label className="stripe-label">
+           Date d'expiration
+          <CardExpiryElement
+            onBlur={handleBlur}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onReady={handleReady}
+            {...createOptions(this.props.fontSize)}
+          />
+        </label>
+        <label className="stripe-label">
+          Cryptogramme visuel
+          <CardCVCElement
+            onBlur={handleBlur}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onReady={handleReady}
+            {...createOptions(this.props.fontSize)}
+          />
+        </label>
+        <button>Payer</button>
+      </form>
+    );
+  }
+}
+const SplitForm = injectStripe(_SplitForm);
+
+
+export default class Checkout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      elementFontSize: window.innerWidth < 450 ? '14px' : '18px',
+    };
+    window.addEventListener('resize', () => {
+      if (window.innerWidth < 450 && this.state.elementFontSize !== '14px') {
+        this.setState({elementFontSize: '14px'});
+      } else if (
+        window.innerWidth >= 450 &&
+        this.state.elementFontSize !== '18px'
+      ) {
+        this.setState({elementFontSize: '18px'});
+      }
+    });
+  }
+
+  render() {
+    const {elementFontSize} = this.state;
+    return (
+      <div>
+        <h4 className="mb-3">Paiement</h4>
+        <div className="form-container">
+          <Elements>
+            <SplitForm fontSize={elementFontSize} />
+          </Elements>
+        </div>
+      </div>
+    );
+  }
+}
