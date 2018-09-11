01a37a26a60ed042d2170c4b519429f90f5324a6
katre <0mica4420@gmail.com>
2018-09-11 14:02:07 +0200

    clean unnecessary comments

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -20,10 +20,8 @@ export const getRate = (birthDate) => {
 }
 
 export const isEmailValid = (email) => {
+  // Keep it simple. Regex can never be trusted 100% But you can send a validation email. 
   var re = /\S+@\S+\.\S+/;
   return re.test(email);
-  // Keep it simple. Regex can never be trusted 100% But you can send a validation email. 
-  // const re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-  // return email.match(re) === true;
 }
 
