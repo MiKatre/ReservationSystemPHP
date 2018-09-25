96a29853332152ce494ecebba9b76994f5572959
katre <0mica4420@gmail.com>
2018-09-25 13:57:48 +0200

    Add functional test
    "testFormPageIsUp"

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -17,4 +17,10 @@ class DefaultControllerTest extends WebTestCase
     $this->assertGreaterThan(0, $crawler->filter('html:contains("Billetterie")')->count());
     $this->assertGreaterThan(0, $crawler->filter('a:contains("Commander")')->count());
   }
+
+  public function testFormPageIsUp(){
+    $client = static::createClient();
+    $client->request('GET', '/form');
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+  }
 }
