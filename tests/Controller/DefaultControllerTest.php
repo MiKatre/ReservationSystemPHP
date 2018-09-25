67dbf10ed4a3f2d17aa16a406886000421f4be4b
katre <0mica4420@gmail.com>
2018-09-25 13:19:55 +0200

    Add assertion to homepage functional test

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -7,10 +7,15 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class DefaultControllerTest extends WebTestCase
 {
   public function testHomepage() {
+
+    // Test status code
     $client = static::createClient();
-
     $client->request('GET', '/');
-
     $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    // Check page content
+    $crawler = $client->request('GET', '/');
+    $this->assertGreaterThan(0, $crawler->filter('html:contains("Billetterie")')->count());
+    
   }
 }
